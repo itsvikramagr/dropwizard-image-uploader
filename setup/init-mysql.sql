130690f28b9abfe97d1a7839226a5b99557ee74a
Vikram Agrawal <vagrawal@qubole.com>
2016-07-17 22:42:08 +0530

    setup

---
+++ setup/init-mysql.sql
@@ -0,0 +1,25 @@
+create database if not exists datastore;
+
+CREATE TABLE if not exists `photo` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `filename` text,
+    `width` int(11) DEFAULT NULL,
+    `length` int(11) DEFAULT NULL,
+    `size` int(11) DEFAULT NULL,
+    `created_at` datetime DEFAULT NULL,
+    `updated_at` datetime DEFAULT NULL,
+     PRIMARY KEY (`id`));
+
+CREATE TABLE if not exists `processed_photo` ( `id` int(11) NOT NULL AUTO_INCREMENT, 
+  `photo_id` int(11) NOT NULL,
+  `host_name` varchar(40) DEFAULT NULL,
+  `image_local_location` varchar(255) DEFAULT NULL,
+  `original_image_uri` varchar(255) DEFAULT NULL,
+  `processed_image_uri` varchar(255) DEFAULT NULL,
+  `is_processed` tinyint(1) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+   PRIMARY KEY (`id`),
+   KEY `host_name` (`host_name`),
+   KEY `photo_id` (`photo_id`) );
+
